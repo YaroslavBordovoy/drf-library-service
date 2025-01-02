e5104b8ea7051c10793c85ab6110aee53b630b02
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-02 16:01:26 +0200

    upgrade user model, add first_name and last_name

--- accounts/models.py
+++ accounts/models.py
@@ -38,8 +38,10 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=63)
+    last_name = models.CharField(_("last name"), max_length=63)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
