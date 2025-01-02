11bbff6ec5be1aab6a7756823b8aac471900ba15
Alex <delete.rap.stupid@gmail.com>
2025-01-02 16:27:35 +0100

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party applications
+    "django_filters",
+    # project application
+    "borrowing_service",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+}
