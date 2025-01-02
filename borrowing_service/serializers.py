76893951d718f2e222e5a21ebf3726c1df374c64
Alex <delete.rap.stupid@gmail.com>
2025-01-02 16:29:32 +0100

    add serializers for Borrowing

---
+++ borrowing_service/serializers.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from django.db import transaction
+from rest_framework import serializers
+
+from borrowing_service.models import Borrowing
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book = serializers.CharField(
+        source="book_service.book.name", read_only=True
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+        )
+
+
+class BorrowingDetailSerializer(serializers.ModelSerializer):
+    # book = BookDetailSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+        )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs=attrs)
+        Borrowing.validate_borrowing(
+            attrs["borrow_date"], attrs["expected_return_date"]
+        )
+        return data
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "expected_return_date")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            book = validated_data.get("book")
+            if book.inventory == 0:
+                raise serializers.ValidationError(
+                    {"book": "This book is out of stock now"}
+                )
+            book.inventory -= 1
+            book.save()
+            borrowing = Borrowing.objects.create(**validated_data)
+            return borrowing
+
+
+class BorrowingReturnSerializer(serializers.ModelSerializer):
+    actual_return_date = serializers.DateField(
+        default=datetime.now, read_only=True
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "actual_return_date")
