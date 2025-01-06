5308c19af003851e8bb02b2678217993a9a1de57
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-06 15:32:34 +0200

    Ruff v3

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY", "django-insecure-qatn#nho^ij!r^-wzz%rj7c&ss&mv0&ti0qg)&ia$7zc6((kjn"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
