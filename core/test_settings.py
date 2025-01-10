dd7efac23cb3f16fd9c293c309eadbe7d87b785e
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-10 22:14:35 +0200

    Fix workflows v6

--- core/test_settings.py
+++ core/test_settings.py
@@ -1,34 +1,10 @@
-import os
+from core.settings import *
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
 
 SECRET_KEY = "test-secret-key"
+
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
-
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    # 3rd party applications
-    "django_filters",
-    "rest_framework",
-    "rest_framework_simplejwt",
-    "drf_spectacular",
-    "django_celery_beat",
-    # project application
-    "borrowing_service",
-    "accounts",
-    "books_service",
-    "payments_service",
-    "telegram_bot",
-]
 
 DATABASES = {
     "default": {
